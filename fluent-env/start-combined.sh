52de62493a38343348d70727b9895ce6453f2766
njfio <7220+njfio@users.noreply.github.com>
2024-08-22 19:01:48 -0400

    gunicorn execution update to use asyn

--- fluent-env/start-combined.sh
+++ fluent-env/start-combined.sh
@@ -3,7 +3,7 @@
 # Start Neo4j in the background
 neo4j start &
 
-gunicorn -w 2 app:app -b 0.0.0.0:5000 &
+gunicorn -w 2 -b 0.0.0.0:5000 --worker-class gevent --timeout 9000 app:app &
 
 echo "started neo4j and web server"
 
